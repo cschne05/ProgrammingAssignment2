46c562943dac50c59f563b78a41a4b3ba9bafe50
Corinne <cschne@sas.upenn.edu>
2014-07-27 12:14:36 -0400

    Added logic determine if inverse is in cache and, if so, return inverse

--- cachematrix.R
+++ cachematrix.R
@@ -23,5 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        n <- x$getInverse()
+        if(!is.null(n)) {
+                message("getting cached data")
+                return(n)
+        }
+        
+        
 }
